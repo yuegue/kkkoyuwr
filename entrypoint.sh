cac0fed5d7d7f0a417bad5c0ffc9b94ae2197f42
yuegue <113776937+yuegue@users.noreply.github.com>
2022-11-12 08:28:43 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vless_UUID="75930cf8-4636-486b-83ea-4b9422c7ecc7"
 fi
 echo ${Vless_UUID}
 
@@ -22,7 +22,7 @@ fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vmess_UUID="75930cf8-4636-486b-83ea-4b9422c7ecc7"
 fi
 echo ${Vmess_UUID}
 
